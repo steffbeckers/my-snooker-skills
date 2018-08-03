7a0d02b5ae495cfd5b756cd055f7e8446d8188fb
Steff Beckers <steff@steffbeckers.eu>
2018-08-03 18:24:02 +0200

    Configure to host front-end on endpoint

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,9 +1,8 @@
 'use strict';
 
 module.exports = function(server) {
-  // Install a `/` route that returns server status
+  // Install a `/api` route that returns server status
   var router = server.loopback.Router();
-  router.get('/', server.loopback.status());
-  server.set('view engine', 'ejs');
+  router.get('/api', server.loopback.status());
   server.use(router);
 };
