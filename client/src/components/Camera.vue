3f0dbefba0de3a0a59c2569435201f18172ec9c3
Steff Beckers <steff@steffbeckers.eu>
2018-08-06 18:13:27 +0200

    Camera Test

--- client/src/components/Camera.vue
+++ client/src/components/Camera.vue
@@ -3,7 +3,7 @@
     <v-slide-y-transition mode="out-in">
       <v-layout column align-center>
         <v-flex>
-          <div class="title">Camera</div>
+          <div class="title">Camera Test</div>
         </v-flex>
       </v-layout>
     </v-slide-y-transition>
