b99c2acf1d1f5ba6edc76909ebc78563eb499f0c
Steff Beckers <steff@steffbeckers.eu>
2018-08-17 01:30:17 +0200

    API explorer activated

--- server/component-config.production.js
+++ server/component-config.production.js
@@ -1,17 +1,8 @@
 'use strict';
 
 module.exports = {
-  'loopback-component-explorer': null,
   '@mean-expert/loopback-component-realtime': {
     debug: false,
-    auth: false,
-    driver: {
-      name: 'socket.io',
-      options: {},
-    },
-    modules: [
-      'IO',
-    ],
     secure: true,
   },
 };
