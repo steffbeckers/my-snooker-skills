9069fe458d6da1b2a1393370c8896c3a8ba271e4
Steff Beckers <steff@steffbeckers.eu>
2018-08-19 22:21:05 +0200

    Hash added to config paths

--- server/config.production.js
+++ server/config.production.js
@@ -1,6 +1,6 @@
 'use strict';
 
 module.exports = {
-  confirmHrefApp: 'https://app.mysnookerskills.com/account/confirm',
-  resetPasswordRequestHrefApp: 'https://app.mysnookerskills.com/account/reset-password',
+  confirmHrefApp: 'https://app.mysnookerskills.com/#/account/confirm',
+  resetPasswordRequestHrefApp: 'https://app.mysnookerskills.com/#/account/reset-password',
 };
