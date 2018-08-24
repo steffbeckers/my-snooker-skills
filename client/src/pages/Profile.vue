3c5d4a106a80af90625923eeabac9cd2a93780a9
Steff Beckers <steff@steffbeckers.eu>
2018-08-24 20:01:46 +0200

    Profile section updates

--- client/src/pages/Profile.vue
+++ client/src/pages/Profile.vue
@@ -1,9 +1,13 @@
 <template>
   <v-container grid-list-lg fluid>
-    <v-layout row wrap>
-      <v-flex xs12>
-        <p class="title">Profile of ...</p>
-      </v-flex>
+    <v-layout class="pt-2" align-center justify-space-around wrap>
+      <v-avatar size="90px" color="primary">
+        <img v-if="$store.state.user && $store.state.user.profilePicture" :src="$store.state.user.profilePicture">
+        <v-icon style="font-size: 42px;" dark v-else>person</v-icon>
+      </v-avatar>
+    </v-layout>
+    <v-layout class="headline mt-3" align-center justify-space-around wrap>
+      {{ $store.state.user.firstName }} {{ $store.state.user.lastName }}
     </v-layout>
   </v-container>
 </template>
