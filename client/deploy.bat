4778e3c00c58a72186404648651b45e57bca749b
Steff Beckers <steff@steffbeckers.eu>
2018-10-05 18:53:42 +0200

    Extra configuration updates before first deployment

---
+++ client/deploy.bat
@@ -0,0 +1,7 @@
+@echo off
+call npm run build
+echo Push the build live.
+call pscp.exe -r C:\dev\mysnookerskills\app\client\dist\* root@dev.steffbeckers.eu:/var/www/mysnookerskills.com/app/public/
+echo Snooker app deployed to app.mysnookerskills.com!
+start "" https://app.mysnookerskills.com/
+pause
