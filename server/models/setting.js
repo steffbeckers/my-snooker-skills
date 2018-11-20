242e486f3e6bea400c84c685e657757b56fd1508
Steff Beckers <steff@steffbeckers.eu>
2018-11-20 13:30:43 +0100

    Settings model updated

--- server/models/setting.js
+++ server/models/setting.js
@@ -1,5 +1,15 @@
 'use strict';
 
 module.exports = function(Setting) {
-
+  // Set createdBy, updatedBy before saving the model
+  Setting.observe('before save', function setAutoData(context, next) {
+    if (context.instance) {
+      if (context.isNewInstance) {
+        context.instance.createdBy = context.options.accessToken.userId;
+        context.instance.ownerId = context.options.accessToken.userId;
+      }
+      context.instance.updatedBy = context.options.accessToken.userId;
+    }
+    next();
+  });
 };
