5a38289ead54c958216ad5855746324bd757f523
Steff Beckers <steff@steffbeckers.eu>
2018-11-23 00:16:27 +0100

    Players loading component

--- client/src/pages/Players.vue
+++ client/src/pages/Players.vue
@@ -1,28 +1,33 @@
 <template>
   <div>
-    <v-toolbar color="transparent" class="elevation-0">
-      <v-toolbar-title color="grey">
-        <v-icon class="mr-2">people</v-icon> Players
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn icon>
-        <v-icon color="rgba(0,0,0,.54)">search</v-icon>
-      </v-btn>
-      <v-btn icon>
-        <v-icon color="rgba(0,0,0,.54)">more_vert</v-icon>
-      </v-btn>
-    </v-toolbar>
-    <v-container class="pt-2" grid-list-lg fluid>
-      <PlayersCardList v-if="listType === 'cards'" :players="players"></PlayersCardList>
-    </v-container>
+    <Loader v-if="!players"></Loader>
+    <div v-if="players">
+      <v-toolbar color="transparent" class="elevation-0">
+        <v-toolbar-title color="grey">
+          <v-icon class="mr-2">people</v-icon> Players
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn icon>
+          <v-icon color="rgba(0,0,0,.54)">search</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon color="rgba(0,0,0,.54)">more_vert</v-icon>
+        </v-btn>
+      </v-toolbar>
+      <v-container class="pt-2" grid-list-lg fluid>
+        <PlayersCardList v-if="listType === 'cards'" :players="players"></PlayersCardList>
+      </v-container>
+    </div>
   </div>
 </template>
 
 <script>
+import Loader from '../components/Loader.vue'
+
 export default {
   data() {
     return {
-      players: [],
+      players: null,
       listType: "cards"
     };
   },
@@ -37,6 +42,9 @@ export default {
           this.players = response.data;
         });
     }
+  },
+  components: {
+    Loader
   },
   name: "Players"
 };
