b9932476ac4201ed78ef817320f7d096e5d17f66
Steff Beckers <steff@steffbeckers.eu>
2019-01-06 18:11:00 +0100

    api config for test env

---
+++ server/config.testing.js
@@ -0,0 +1,6 @@
+'use strict';
+
+module.exports = {
+  confirmHrefApp: 'https://test.mysnookerskills.com/account/confirm',
+  resetPasswordRequestHrefApp: 'https://test.mysnookerskills.com/account/reset-password',
+};
