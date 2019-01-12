4326b09209b844acfc47981997249d27847e9f0b
Steff Beckers <steff@steffbeckers.eu>
2019-01-13 00:18:45 +0100

    application--wrap main.v-content top toolbar nav height styling fix on SM breakpoint auto resize

--- client/src/App.vue
+++ client/src/App.vue
@@ -370,9 +370,19 @@
   </v-app>
 </template>
 
-<style scoped>
-main.content {
-  margin-bottom: 60px;
+<style>
+body {
+  padding-top: constant(safe-area-inset-top);
+  padding-top: env(safe-area-inset-top);
+}
+
+/* Top navigation toolbar height fix */
+div.application--wrap nav.v-toolbar div.v-toolbar__content {
+  height: 56px !important;
+}
+div.application--wrap > main.v-content {
+  padding-top: 56px !important;
+  margin-bottom: 10px;
 }
 
 .v-footer {
@@ -495,14 +505,3 @@ export default {
   name: 'App'
 }
 </script>
-
-<style>
-body {
-  padding-top: constant(safe-area-inset-top);
-  padding-top: env(safe-area-inset-top);
-}
-.footer {
-  margin-bottom: constant(safe-area-inset-bottom);
-  margin-bottom: env(safe-area-inset-bottom);
-}
-</style>
