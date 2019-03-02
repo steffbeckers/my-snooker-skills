a496a5fd34c6b4c7bfbffeb4f620be9e34113e74
Steff Beckers <steff@steffbeckers.eu>
2019-03-02 02:06:11 +0100

    Updates on docker-compose and datasource mongodb container

--- server/datasources.development.js
+++ server/datasources.development.js
@@ -6,7 +6,7 @@ module.exports = {
     name: 'mongodb',
     host: 'mongodb',
     port: 27017,
-    user: 'root',
+    user: 'MySnookerSkills',
     password: process.env.MYSNOOKERSKILLS_MONGODB_DEV_PASSWORD,
     database: 'MySnookerSkills',
   },
